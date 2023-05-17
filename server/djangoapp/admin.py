0a58360ac6127259a2a7d305f50829139ceafc2f
riswanmsm <msmr1986@gmail.com>
2023-05-17 17:54:45 +0530

    updated admin site with carmake and carmodel

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,28 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarModel, CarMake
 
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 3
 
 # CarModelAdmin class
 
+
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
 # CarMakeAdmin class with CarModelInline
 
+
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ['name']
+
+
 # Register models here
+admin.site.register(CarModel, CarModelAdmin)
+admin.site.register(CarMake, CarMakeAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,6 +9,13 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    description = models.CharField(max_length=500, blank=False)
+    # description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -20,8 +27,27 @@ from django.utils.timezone import now
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 
+class CarModel(models.Model):
+    CAR_TYPES = [
+        ('sedan', 'Sedan'),
+        ('suv', 'SUV'),
+        ('wagon', 'Wagon'),
+    ]
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, blank=False)
+    dealer_id = models.IntegerField()
+    type = models.CharField(max_length=10, choices=CAR_TYPES)
+    year = models.DateField()
+
+    def __str__(self):
+        return self.name
+
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarDealer:
+    pass
 
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+class DealerReview:
+    pass
